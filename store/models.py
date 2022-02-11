3f4cfd24636319d7b90cab5bcc29d55247e94d52
Ameeruddin06 <ameerb0601@gmail.com>
2022-02-11 19:38:50 +0530

    Products Details- Mini-Proj06

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from distutils.command.upload import upload
 from unicodedata import category
 from django.db import models
 from category.models import Category
+from django.urls import reverse
 # Create your models here.
 class Product(models.Model):
     product_name = models.CharField(max_length=200, unique=True)
@@ -14,6 +15,9 @@ class Product(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
+
+    def get_url(self):
+        return reverse('product_detail',args=[self.category.slug, self.slug])
 
     def __str__(self):
         return self.product_name
