2fe65e8c88b162128c94492195ae19faf0d8c310
Raquib Khan <khanraquib01@gmail.com>
2022-03-02 20:32:34 +0530

    created and configured cart app and added cart templates

---
+++ carts/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from store.models import Product
+
+class Cart(models.Model):
+	card_id = models.CharField(max_length=250, blank=True)
+	date_added = models.DateField(auto_now_add=True)
+
+	def __str__(self):
+		return self.card_id
+
+
+class CartItem(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.CASCADE)
+	cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
+	quantity = models.IntegerField()
+	is_active = models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.product
