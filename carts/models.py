685ef14855ffcaea3a03be768bfec76be525c458
Raquib Khan <khanraquib01@gmail.com>
2022-03-03 11:19:38 +0530

    Added Cart using session key

--- carts/models.py
+++ carts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from store.models import Product
 
 class Cart(models.Model):
-	card_id = models.CharField(max_length=250, blank=True)
+	cart_id = models.CharField(max_length=250, blank=True)
 	date_added = models.DateField(auto_now_add=True)
 
 	def __str__(self):
@@ -14,6 +14,9 @@ class CartItem(models.Model):
 	cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
 	quantity = models.IntegerField()
 	is_active = models.BooleanField(default=True)
+	
+	def sub_total(self):
+		return self.product.price * self.quantity
 
 	def __str__(self):
 		return self.product
