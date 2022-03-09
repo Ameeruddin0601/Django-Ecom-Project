f4ebcae53f70706b2363b2d68bd880e6d5bda4d0
0xHuz <skmuhammadhuzaifa@gmail.com>
2022-03-09 10:45:58 +0530

    Email verification. Thank to @Q-usman

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,22 @@
+from functools import reduce
+from shutil import register_unpack_format
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
+import accounts
 from accounts.forms import RegistrationForm
 from django.contrib import messages
 from django.contrib import auth
 from django.contrib.auth.decorators import login_required
 from accounts.models import Account
+from django.http import HttpResponse
+# verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode,urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
 def register(request):
     form = RegistrationForm()
     if request.method=='POST':
@@ -18,6 +31,18 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, userName=username, password=password)
             user.phone_number = phone_number
             user.save()
+            # user activation
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html',{
+                'user':user,
+                'domain':current_site,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)),
+                'token':default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject,message,to=[to_email])
+            send_email.send()
             messages.success(request, 'Registration Successful')
             return redirect('register')
         else:
@@ -48,3 +73,17 @@ def logout(request):
     auth.logout(request)
     messages.success(request, 'You are logged out.')
     return redirect('login')
+def activate(request,uidb64,token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except(TypeError,ValueError,OverflowError,Account.DoesNotExist):
+        user = None
+    if user is not None and default_token_generator.check_token(user,token):
+        user.is_active = True
+        user.save()
+        messages.success(request,'congratulation! Your account is activated')
+        return redirect('login')
+    else:
+        messages.error(request,'invalid activation link')
+        return redirect('register')
