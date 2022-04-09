aebcac8a21bdd0ed6233aec39b5145940868eec9
Raquib Khan <khanraquib01@gmail.com>
2022-04-09 14:56:53 +0530

    Payment Gateway Integrated

--- orders/models.py
+++ orders/models.py
@@ -51,7 +51,8 @@ class Order(models.Model):
         return f'{self.address_line_1} {self.address_line_2}'
 
     def __str__(self):
-        return self.first_name
+        return self.order_number
+		
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     payment = models.ForeignKey(Payment, on_delete=models.SET_NULL, blank=True, null=True)
