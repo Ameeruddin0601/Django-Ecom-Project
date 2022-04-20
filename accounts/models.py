d1bf9e9e57a278f7b284f10b1c79beee99eab432
0xHuz <skmuhammadhuzaifa@gmail.com>
2022-04-20 15:05:31 +0530

    I don't know what these changes are supposed to accomplish but somebody told me to make them.

--- accounts/models.py
+++ accounts/models.py
@@ -1,18 +1,20 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+
+
 # Create your models here.
 
 class MyAccountManager(BaseUserManager):
-    def create_user(self, first_name, last_name, userName, email, password=None):
+    def create_user(self, first_name, last_name, username, email, password=None):
         if not email:
             raise ValueError('User must have an email address')
 
-        if not userName:
-            raise ValueError('User must have a username')
+        if not username:
+            raise ValueError('User must have an username')
 
         user = self.model(
             email = self.normalize_email(email),
-            userName = userName,
+            username = username,
             first_name = first_name,
             last_name = last_name,
         )
@@ -21,15 +23,14 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, first_name, last_name, email, userName, password):
+    def create_superuser(self, first_name, last_name, email, username, password):
         user = self.create_user(
             email = self.normalize_email(email),
-            userName = userName,
-            password= password,
+            username = username,
+            password = password,
             first_name = first_name,
             last_name = last_name,
         )
-
         user.is_admin = True
         user.is_active = True
         user.is_staff = True
@@ -37,25 +38,30 @@ class MyAccountManager(BaseUserManager):
         user.save(using=self._db)
         return user
 
-class Account(AbstractBaseUser):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-    userName = models.CharField(max_length=50,unique=True)
-    email = models.EmailField(max_length=100,unique=True)
-    phone_number = models.CharField(max_length=50)
 
-    #required
-    date_joined = models.DateTimeField(auto_now_add=True)
-    last_login = models.DateTimeField(auto_now_add=True)
-    is_admin = models.BooleanField(default=False)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
+
+class Account(AbstractBaseUser):
+    first_name      = models.CharField(max_length=50)
+    last_name       = models.CharField(max_length=50)
+    username        = models.CharField(max_length=50, unique=True)
+    email           = models.EmailField(max_length=100, unique=True)
+    phone_number    = models.CharField(max_length=50)
+
+    # required
+    date_joined     = models.DateTimeField(auto_now_add=True)
+    last_login      = models.DateTimeField(auto_now_add=True)
+    is_admin        = models.BooleanField(default=False)
+    is_staff        = models.BooleanField(default=False)
+    is_active        = models.BooleanField(default=False)
+    is_superadmin        = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['userName','first_name','last_name']
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     objects = MyAccountManager()
+
+    def full_name(self):
+        return f'{self.first_name} {self.last_name}'
 
     def __str__(self):
         return self.email
@@ -66,4 +72,18 @@ class Account(AbstractBaseUser):
     def has_module_perms(self, add_label):
         return True
 
-    
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(Account, on_delete=models.CASCADE)
+    address_line_1 = models.CharField(blank=True, max_length=100)
+    address_line_2 = models.CharField(blank=True, max_length=100)
+    profile_picture = models.ImageField(blank=True, upload_to='userprofile')
+    city = models.CharField(blank=True, max_length=20)
+    state = models.CharField(blank=True, max_length=20)
+    country = models.CharField(blank=True, max_length=20)
+
+    def __str__(self):
+        return self.user.first_name
+
+    def full_address(self):
+        return f'{self.address_line_1} {self.address_line_2}'
